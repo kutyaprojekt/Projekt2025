7dc50bb8bfea6d314b9a1fbc2efaef9d618f49a6
allatprojekt <projekt99001@gmail.com>
2025-03-24 22:48:53 +0100

    proba

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/allatradar", // github
+  base:"/Projekt2025", // github
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
